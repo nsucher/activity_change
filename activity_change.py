aa27b5bb887f7c3cdcaf3100687b6a4b81903d73
nsucher <96442661+nsucher@users.noreply.github.com>
2023-02-07 14:17:21 -0800

    Add files via upload

--- activity_change.py
+++ activity_change.py
@@ -6,7 +6,7 @@
 # plot_change.py: Generate significance and positive and negative mean differences of electrode weight changes at neuroanatomical sites
 
 # OUTPUT:
-#       Neurosemiology.xlsx (pvals per ROI)
+#       neurosem_pvals.xlsx (pvals per ROI)
 #       pos_plot_change.xlsx (positive electrical activity compared to the mean)
 #       neg_plot_change.xlsx (negative ")
 
@@ -247,13 +247,24 @@ def write_new_sheet(sxmx_input, ptsz_i, ptsz_name, workbook_name):
 
         workbook_name.save(opscea_data_path + name_xlsx)
         workbook_name.close()
+def find_laterality(em_sign_row):
+    isR = np.nansum(em_sign_row) > 0
+    isL = isR != 1
+
+    if isR:
+        laterality = 'r'
+    elif isL:
+        laterality = 'l'
+
+    return laterality
+
 
 print('help I am stuck in the code')
 print(ptsz_input,' ',sxmx_input)
 
 #   INITIALIZE NAMES
 pt_name, sz_name, sx_name, mx_name, ptsz_name, pt_path, sz_path = input_names(ptsz_input,sxmx_input,avg_path)
-list_str_xlsx = ['Neurosemiology.xlsx','pos_sign_change.xlsx','neg_sign_change.xlsx']
+list_str_xlsx = ['neurosem_pvals.xlsx', 'all_sign_change.xlsx', 'pos_sign_change.xlsx', 'neg_sign_change.xlsx', 'elec_w8s.xlsx']
 
 #   OPSCEA DATA PATH
 os.chdir(avg_path + '/..')
@@ -264,12 +275,15 @@ for str_xlsx in range(0, len(list_str_xlsx)):
     create_workbooks(list_str_xlsx[str_xlsx])
 create_em_workbook('elec_matrix.xlsx')
 
-workbook_loaded_pv = load_workbook(filename='Neurosemiology.xlsx')
+
+workbook_loaded_pv = load_workbook(filename='neurosem_pvals.xlsx')
+workbook_loaded_all = load_workbook(filename='all_sign_change.xlsx')
 workbook_loaded_pos = load_workbook(filename='pos_sign_change.xlsx')
 workbook_loaded_neg = load_workbook(filename='neg_sign_change.xlsx')
 workbook_loaded_em = load_workbook(filename='elec_matrix.xlsx')
+workbook_loaded_w8s = load_workbook(filename='elec_w8s.xlsx')
 
-workbook_list = [workbook_loaded_pv, workbook_loaded_pos, workbook_loaded_neg] # elec matrix not included
+workbook_list = [workbook_loaded_pv, workbook_loaded_all, workbook_loaded_pos, workbook_loaded_neg, workbook_loaded_w8s] # elec matrix not included
 
 #   CREATE NEW SHEET OF SYMPTOM/MODE
 for w_i in range(0, len(workbook_list)):
@@ -289,7 +303,6 @@ del df_params
 #   GET ANATOMY LABELS
 anat, em = load_elecs(pt_path)
 
-#   FILL IN  XLSX OF 3D ELEC MATRIX
 sheet_em = workbook_loaded_em.active
 
 #       SEIZURE NAMES
@@ -298,97 +311,143 @@ for each_col1 in range(0, np.shape(em)[1]):
 
 # szxyz = []
 #       3D COORDINATES
-for em_row in range(0, np.shape(em)[0]):
-    for em_col in range(0, np.shape(em)[1]):
-        write_cell(em_row + 2, 3 * ptsz_i - 1 + em_col, em[em_row][em_col], sheet_em)
+# for em_row in range(0, np.shape(em)[0]):
+#     for em_col in range(0, np.shape(em)[1]):
+#         write_cell(em_row + 2, 3 * ptsz_i - 1 + em_col, em[em_row][em_col], sheet_em)
+#
+#
+# em_sign_row = em[:,]
+# laterality = find_laterality(em_sign_row)
 
-print(np.nansum(em[:, 2]))
-# isR = np.nansum(em[:, 2]) > 0
-# print(isR)
 
-# #   PTSZ PATH
-# os.chdir(pt_path + ptsz_name + '/')
-#
-# #   LOAD MAT FILES
-# mat_name, badch_name, csv_name = file_suffix(ptsz_name)
-# fs, d = sz_mat(mat_name) #fs = frame speed; d = ppEEG
-# badch = badch_mat(badch_name)
-# sz_count = round(sz_count)
-#
-# # OPSCEA DATA PATH
-# os.chdir(avg_path + '..')
-# opscea_data_path = os.getcwd() + '/'
-#
-# # FILL IN XLSX WITH ROI LABELS
+#   PTSZ PATH
+os.chdir(pt_path + ptsz_name + '/')
+
+#   LOAD MAT FILES
+mat_name, badch_name, csv_name = file_suffix(ptsz_name)
+fs, d = sz_mat(mat_name) #fs = frame speed; d = ppEEG
+badch = badch_mat(badch_name)
+sz_count = round(sz_count)
+
+# OPSCEA DATA PATH
+os.chdir(avg_path + '..')
+opscea_data_path = os.getcwd() + '/'
+
+# FILL IN XLSX WITH ROI LABELS
 # for w_i in range(0, len(workbook_list)):
 #     workbook_name = workbook_list[w_i]
 #     w_i_sheet_sxmx = workbook_name[sxmx_input]
 #
 #     for n_l in range(0, len(neuroanat_list)):
 #         write_cell(n_l+2, 1, neuroanat_list[n_l], w_i_sheet_sxmx)
-#
-# # CREATE GOOD CHANNEL LIST FOR LOGICAL, PPEEG (AKA D), AND ANATOMY
-# for a_i in range(0,np.shape(anat)[0]):
-#
-#     if np.size(anat[a_i]) > 0:
-#         if anat[a_i][0] not in neuroanat_list:
-#
-#             good_logical, good_d, good_anat, good_em = good_ch(badch, d, anat, em, a_i)
-#
-# os.chdir(pt_path + ptsz_name + '/')
-#
-# sx_csv = pd.read_csv(csv_name, usecols=[sx_name])
-# sx_vec = sx_csv.values.tolist()
-#
-# pval = np.empty(np.shape(neuroanat_list))
-# pval[:] = np.NaN
-#
-# if np.float64(mx_name) in sx_vec:
-#     filt_d = filt(good_d, fs)  # Filter out < 1 Hz (and up to nyquist)
-#     LL = ll_transform(llw, fs, filt_d, bl_start, bl_stop)   # ~8.5 seconds for EC96_01
-#     first_mx = sx_vec.index(np.float64(mx_name))
-#     at_onset, before_onset, after_onset, time_ms = sx_onset(fs, perdur_input, first_mx)
-#     LL_meandiff = sem_w8s(LL, at_onset, before_onset, after_onset)
-#
-#     pval_list = np.empty(np.shape(neuroanat_list))
-#     pval_list[:] = np.NaN
-#
-#     anat_index = [np.NaN] * np.shape(good_anat)[0]
-#     anat_list = np.empty(np.shape(good_anat)).tolist()
-#
-#     for g in range(0, np.shape(good_anat)[0]):
-#         if len(good_anat[g]) > 0:
-#             anat_list[g] = good_anat[g][0]
-#
-#     for n_l in range(0, len(neuroanat_list)):
-#         anat_w8s = []
-#         anat_w8s_label = []
-#         neg_change = []
-#
-#         # print(neuroanat_list[n_l])
-#         for a_i in range(0, len(anat_list)):
-#             if neuroanat_list[n_l] == anat_list[a_i][:]:
-#                 anat_w8s_label.append(neuroanat_list[n_l])
-#                 anat_w8s.append(LL_meandiff[a_i])
-#                 activity_change = float(LL_meandiff[a_i])
-#                 if activity_change > 0:
-#                     pos_sheet_sxmx = workbook_loaded_pos[sxmx_input]
-#                     write_cell(n_l + 2, ptsz_i + 1, activity_change, pos_sheet_sxmx)
-#                 elif activity_change < 0:
-#                     neg_sheet_sxmx = workbook_loaded_neg[sxmx_input]
-#                     write_cell(n_l + 2, ptsz_i + 1, activity_change, neg_sheet_sxmx)
-#         tstat, pval = ttest_1samp(anat_w8s, 0)
-#         sheet_sxmx = workbook_loaded_pv[sxmx_input]
-#         write_cell(n_l + 2, ptsz_i + 1, pval, sheet_sxmx)
-# else:
-#     pass
 
-workbook_loaded_pv.save(opscea_data_path + 'Neurosemiology.xlsx')
+
+pv_sheet = workbook_loaded_pv[sxmx_input]
+for n_l in range(0, len(neuroanat_list)):
+    write_cell(n_l+2, 1, neuroanat_list[n_l], pv_sheet)
+
+
+# CREATE GOOD CHANNEL LIST FOR LOGICAL, PPEEG (AKA D), AND ANATOMY
+for a_i in range(0,np.shape(anat)[0]):
+
+    if np.size(anat[a_i]) > 0:
+        if anat[a_i][0] not in neuroanat_list:
+
+            good_logical, good_d, good_anat, good_em = good_ch(badch, d, anat, em, a_i)
+
+#   FILL IN  XLSX OF 3D ELEC MATRIX
+sheet_em = workbook_loaded_em.active
+
+# print(good_anat)
+#       3D COORDINATES
+for em_row in range(0, np.shape(good_em)[0]):
+    # print(good_anat[em_row][0])
+    write_cell(em_row + 2, 1, good_anat[em_row][0], sheet_em)
+    for em_col in range(0, np.shape(good_em)[1]):
+        write_cell(em_row + 2, 3 * ptsz_i - 1 + em_col, good_em[em_row][em_col], sheet_em)
+
+
+em_sign_row = good_em[:,]
+laterality = find_laterality(em_sign_row)
+
+os.chdir(pt_path + ptsz_name + '/')
+
+sx_csv = pd.read_csv(csv_name, usecols=[sx_name])
+sx_vec = sx_csv.values.tolist()
+
+pval = np.empty(np.shape(neuroanat_list))
+pval[:] = np.NaN
+
+if np.float64(mx_name) in sx_vec:
+    filt_d = filt(good_d, fs)  # Filter out < 1 Hz (and up to nyquist)
+    LL = ll_transform(llw, fs, filt_d, bl_start, bl_stop)   # ~8.5 seconds for EC96_01
+    first_mx = sx_vec.index(np.float64(mx_name))
+    at_onset, before_onset, after_onset, time_ms = sx_onset(fs, perdur_input, first_mx)
+    LL_meandiff = sem_w8s(LL, at_onset, before_onset, after_onset)
+
+    pval_list = np.empty(np.shape(neuroanat_list))
+    pval_list[:] = np.NaN
+
+    anat_index = [np.NaN] * np.shape(good_anat)[0]
+    anat_list = np.empty(np.shape(good_anat)).tolist()
+
+    for g in range(0, np.shape(good_anat)[0]):
+        if len(good_anat[g]) > 0:
+            anat_list[g] = good_anat[g][0]
+
+    for electrode in range(0,np.shape(LL_meandiff)[0]):
+        activity_change = float(LL_meandiff[electrode])
+        all_sheet_sxmx = workbook_loaded_all[sxmx_input]
+        if activity_change < 400 and activity_change > -400:
+            write_cell(electrode + 2, ptsz_i + 1, activity_change, all_sheet_sxmx)
+            if activity_change > 0:
+                pos_sheet_sxmx = workbook_loaded_pos[sxmx_input]
+                write_cell(electrode + 2, ptsz_i + 1, activity_change, pos_sheet_sxmx)
+            elif activity_change < 0:
+                neg_sheet_sxmx = workbook_loaded_neg[sxmx_input]
+                write_cell(electrode + 2, ptsz_i + 1, activity_change, neg_sheet_sxmx)
+
+    w8s_array = []
+    anat_array = []
+    for n_l in range(0, len(neuroanat_list)):
+        anat_w8s = []
+        anat_w8s_label = []
+
+        # sheet_w8s = workbook_loaded_w8s[sxmx_input]
+        # print(neuroanat_list[n_l])
+        for a_i in range(0, len(anat_list)):
+            if neuroanat_list[n_l] == anat_list[a_i][:]:
+                if LL_meandiff[a_i] < 400:
+                    if LL_meandiff[a_i] > -400:
+                        anat_w8s_label.append(neuroanat_list[n_l])
+                        anat_w8s.append(LL_meandiff[a_i])
+                        sheet_w8s = workbook_loaded_w8s[sxmx_input]
+                        write_cell(a_i + 2, 1, anat_w8s_label[-1], sheet_w8s)
+                        write_cell(a_i + 2, ptsz_i+1, anat_w8s[-1], sheet_w8s)
+                # activity_change = float(LL_meandiff[a_i])
+                # all_sheet_sxmx = workbook_loaded_all[sxmx_input]
+        sheet_w8s = workbook_loaded_w8s[sxmx_input]
+        w8s_array.append(anat_w8s)
+        anat_array.append(anat_w8s_label)
+        # write_cell(a_i + 2, ptsz_i + 1, w8s_array[-1], sheet_w8s)
+        tstat, pval = ttest_1samp(anat_w8s, 0)
+        sheet_sxmx = workbook_loaded_pv[sxmx_input]
+        write_cell(n_l + 2, ptsz_i + 1, pval, sheet_sxmx)
+
+else:
+    pass
+
+
+workbook_loaded_pv.save(opscea_data_path + 'neurosem_pvals.xlsx')
+workbook_loaded_all.save(opscea_data_path + 'all_sign_change.xlsx')
 workbook_loaded_pos.save(opscea_data_path + 'pos_sign_change.xlsx')
 workbook_loaded_neg.save(opscea_data_path + 'neg_sign_change.xlsx')
 workbook_loaded_em.save(opscea_data_path + 'elec_matrix.xlsx')
+workbook_loaded_w8s.save(opscea_data_path + 'elec_w8s.xlsx')
 
 workbook_loaded_pv.close()
+workbook_loaded_all.close()
 workbook_loaded_pos.close()
 workbook_loaded_neg.close()
 workbook_loaded_em.close()
+workbook_loaded_w8s.close()
--- neurosem_data.m
+++ neurosem_data.m
@@ -1,5 +1,6 @@
 % Natalia Sucher in the Kleen Lab, UCSF
-% 1/31/2023
+% Created 1/31/2023
+% Edited 2/7/2023
 
 
 tic
@@ -12,13 +13,17 @@ avg_path= [opscea_path 'OPSCEADATA/avg_change_folders/'];   %path for parameters
 
 % EDIT THIS TO REFLECT THE SYMPTOMS, MODES, AND PERDUR (SECONDS BEFORE AND AFTER SYMPTOM ONSET)
 % sx_input = {'lhx','rhx','lud','rud', 'lup','rup'};
-sx_input = {'lhx','rhx'};
+% sx_input = {'lud','rud'};
+sx_input = {'lex','rex'};
+% sx_input = {'lhx','rhx'};
 mx_input = {'2'};
 perdur_input = '15';
 
 % UNCOMMENT AND EDIT THIS IF YOU WANT TO SPECIFY PATIENT SEIZURES MANUALLY
-% manual_ptsz = {'EC91_03','EC96_01','EC107_01','EC133_03', 'EC166_01','EC228_03','EC229_02'}; % specify which patient and seizure
-manual_ptsz = {'EC91_03','EC133_03'}; % specify which patient and seizure
+manual_ptsz = {'EC91_03','EC96_01','EC107_01','EC133_03', 'EC166_01','EC228_03','EC229_02'}; % specify which patient and seizure
+% manual_ptsz = {'EC91_03','EC133_03'}; % specify which patient and seizure
+% manual_ptsz = {'EC228_03'}; % specify which patient and seizure
+
 
 % LOAD RELEVANT PYTHON INFO
 pe = pyenv;
@@ -30,23 +35,14 @@ sxmx_count = 0;
 
 
 % DELETE PREVIOUS XLSX FILES
-cd(avg_path)
-cd ..
-if exist('Neurosemiology.xlsx')
-    delete Neurosemiology.xlsx
-    disp('aha! you nasty rascal.')
-end
+cd([avg_path '..'])
 
-if exist('pos_sign_change.xlsx')
-    delete pos_sign_change.xlsx
-    disp('aha! you positive rascal.')
-end
-
-if exist('neg_sign_change.xlsx')
-    delete neg_sign_change.xlsx
-    disp('aha! you negative rascal.')
-end
-
+delete neurosem_pvals.xlsx
+delete all_sign_change.xlsx
+delete pos_sign_change.xlsx
+delete neg_sign_change.xlsx
+delete elec_matrix.xlsx
+delete elec_w8s.xlsx
 
 cd(opscea_path)
 
@@ -59,73 +55,31 @@ count = 0;
 
 
 for sx_i = 1:length(sx_input) % for loop throughout symptoms
-%     disp(count)
     sx_name = sx_input{sx_i};
 
     for mx_i = 1:length(mx_input) % for loop throughout modes
-%         count = count + 1
         mx_name = mx_input{mx_i};
-
 
         sxmx_name = [sx_name ' ' mx_name];
 
         sxmx_count = sxmx_count + 1;
 
-        if exist('manual_ptsz','var')
-            for ptsz_i = 1:length(manual_ptsz)
+        for ptsz_i = 1:length(manual_ptsz)
 
-                sz_count = sz_count + 1;
+            sz_count = sz_count + 1;
 
-                split_manual_ptsz = split(manual_ptsz{ptsz_i},'_');
-                pt_name = split_manual_ptsz{1};
-                sz_name = split_manual_ptsz{2};
-                ptsz_name = [pt_name '-' sz_name];
+            split_manual_ptsz = split(manual_ptsz{ptsz_i},'_');
+            pt_name = split_manual_ptsz{1};
+            sz_name = split_manual_ptsz{2};
+            ptsz_name = [pt_name '-' sz_name];
 
-                figure; % different figure for each symptom/mode combination
-                title([sxmx_name ' ' ptsz_name]);
+            cd(opscea_path)
+            [laterality, w8s_array, anat_array] = pyrunfile("activity_change.py", ["laterality", "w8s_array", "anat_array"], sxmx_input=sxmx_name, ptsz_input=ptsz_name, perdur_input=perdur_input, avg_path=avg_path, sz_count=sz_count, sxmx_count=sxmx_count, ptsz_i=ptsz_i);
+            
+            activity_plot(string(laterality), w8s_array, anat_array, sxmx_name, ptsz_name, perdur_input, avg_path, opscea_path, sz_count, sxmx_count, ptsz_i, pt_name, sz_name)
 
-                pyrunfile("plot_change.py", sxmx_input=sxmx_name, ptsz_input=ptsz_name, perdur_input=perdur_input, avg_path=avg_path, sz_count=sz_count, sxmx_count=sxmx_count, ptsz_i=ptsz_i);
-%                 disp(toc)
-
-%                 cd(avg_path)
-% 
-%                 % XLSX FILES
-% 
-%                 cd(opscea_path)
-                
-            end
-
-        end % ~exist(manual_pt,'var')
-
-
-                    
-
-% %   2. run plot_change.py 
-
-%         pyrunfile("plot_change.py", sxmx_input=sxmx_name, ptsz_input=ptsz_name)
-
-%              "plot_change.py",pt='EC91',sz=);
-% %       A. input 
-% %           i. patient (ex.EC127)          
-% %           ii. symptom (ex. rhx = right head turn)
-% %           iii. mode (1 = auto, 2 = tonic, 3 = clonic)
-% %           iv. perdur (single number (such as 15) that is a sec window before and after symptom onset)
-% %       B. output 
-% %           i. neurosemiology.xlsx 
-% %   3. run sign_change.py
-% %       A. input
-% %           i. directory
-% %       B. output 
-% %           i. em_LL.xlsx
-% %           ii. output sign_change.xlsx 
-% %           iii. pos_sign_change.xlsx
-% %           iv. neg_sign_change.xlsx
-% %   4. run pval_color.m
-% %       A. input
-% %           i. directory
-% 
-% 
-    end % for mx_i = 1:length(mx_input)
-end % for sx_i = 1:length(sx_input)
+        end
+    end 
+end 
 
 toc
